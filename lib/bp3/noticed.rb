d2b2e81a671224e0d8a31c734ddd7a37b7a13904
Wim den Braven <wimdenbraven@persuavis.com>
2024-05-28 14:40:33 -0700

    Include job/que concerns

--- lib/bp3/noticed.rb
+++ lib/bp3/noticed.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'noticed/version'
+require_relative 'noticed/common_includes'
+require_relative 'noticed/job_includes'
+require_relative 'noticed/que_includes'
+require_relative 'noticed/prepend_perform'
+require_relative 'noticed/railtie'
 
 module Bp3
   module Noticed
